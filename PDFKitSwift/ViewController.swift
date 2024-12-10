99f99d723ad168ab0d2f8313b6b1fd0b4b34e366
jorge.portada <jorge.portada@grupopakar.com.mx>
2024-12-10 16:39:44 -0600

    Initial Commit

---
+++ PDFKitSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PDFKitSwift
+//
+//  Created by Portada Luna Jorge Antonio on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PDFKitSwiftTests/PDFKitSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKitSwiftTests.swift
+//  PDFKitSwiftTests
+//
+//  Created by Portada Luna Jorge Antonio on 10/12/24.
+//
+
+import Testing
+@testable import PDFKitSwift
+
+struct PDFKitSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
