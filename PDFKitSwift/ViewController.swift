5e9b9e5dd140291aca74b0cfdf5e8827a0c71247
jorge.portada <jorge.portada@grupopakar.com.mx>
2024-12-12 13:54:32 -0600

    vista mostrando pdf

--- PDFKitSwift/ViewController.swift
+++ PDFKitSwift/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
 
+    let pdfLector = PDFView()
+    var paginacion = false, displayDireccion = false, autoescala = false
+    
+    @IBOutlet weak var pdfView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pdfLector.delegate = self
+        visualizar()
     }
 
-
+    // agrega vista de pdf a la view
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pdfLector.frame = view.bounds
+        pdfView.addSubview(pdfLector)
+    }
+    
+    @IBAction func btnOrientacionAction(_ sender: Any) {
+    }
+    
+    @IBAction func btnAjustarAction(_ sender: Any) {
+    }
+    
+    @IBAction func btnScrollAction(_ sender: UIBarButtonItem) {
+    }
+    
+    func visualizar() {
+        guard let url = Bundle.main.url(forResource: "ejemplopdf", withExtension: "pdf") else { return }
+        guard let documento = PDFDocument(url: url) else { return }
+        pdfLector.document = documento
+        //pdfLector.autoScales = true
+    }
 }
 
+// MARK:PDFViewDelegate
+extension ViewController: PDFViewDelegate {
+    
+}
